2f4b6f4eb0c0d341c929bc85b503fab59bd26576
javier carreño manrique <javier.carreno.manrique@gmail.com>
2023-07-02 13:06:18 +0200

    Completed web page. First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import React, { useState, useEffect } from 'react';
+import { Carousel } from 'react-responsive-carousel';
 import './App.css';
+import projectsData from './projects.json';
 
 function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    setProjects(projectsData.projects);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Aratani Portfolio</h1>
       </header>
+      <Carousel>
+        {projects.map((project) => (
+          <div key={project.id}>
+            <img src={project.image} alt={project.title} />
+            <h3>{project.title}</h3>
+            <p>{project.description}</p>
+            <a href={project.link} target="_blank" rel="noopener noreferrer">Ver Proyecto</a>
+          </div>
+        ))}
+      </Carousel>
     </div>
   );
 }
